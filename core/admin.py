39b29596767cb7fb167aa9ba30eecdb36458e3ca
biancaa05 <turcub02@gmail.com>
2026-03-31 15:23:55 +0300

    Setup django models for Listing and Report and register in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Listing, Report
 
-# Register your models here.
+# Înregistrăm modelele pentru a fi vizibile în Dashboard
+admin.site.register(Listing)
+admin.site.register(Report)
