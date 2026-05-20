c9173353991d28030c055e0cff5bf4df5e35b4a8
biancaa05 <turcub02@gmail.com>
2026-05-20 17:27:51 +0300

    Add forgot password link to login page

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from core import views
 
@@ -13,4 +14,8 @@ urlpatterns = [
     path('result/<int:listing_id>/', views.result_detail_view, name='result_detail'),
     path('analyze-external/', views.analyze_external, name='analyze_external'),
     path('search_results/', views.search_results, name='search_results'),
+path('reset_password/', auth_views.PasswordResetView.as_view(template_name='core/registration/password_reset_form.html'), name='reset_password'),
+path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='core/registration/password_reset_done.html'), name='password_reset_done'),
+path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='core/registration/password_reset_confirm.html'), name='password_reset_confirm'),
+path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='core/registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
